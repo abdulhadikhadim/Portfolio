154abaa9b15fa74ecdd98a4692b7f489fc06e06a
Abdul Hadi <abhadi023@gmail.com>
2025-12-27 19:05:54 +0500

    add the packages

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-import anime from 'animejs';
+import anime from 'animejs/lib/anime.es.js';
 
 export default function About() {
   const sectionRef = useRef<HTMLDivElement>(null);
