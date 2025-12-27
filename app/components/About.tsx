914a3434d9cb8bc11bbe52113968858343a81ee1
Abdul Hadi <abhadi023@gmail.com>
2025-12-27 19:54:34 +0500

    fix the animejson

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs';
 
 export default function About() {
   const sectionRef = useRef<HTMLDivElement>(null);
